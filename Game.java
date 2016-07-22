60a5c0776b00c326be335c251ee2432e86685739
willcurry <willcurry@Wills-MBP.default>
2016-07-22 16:38:30 +0100

    Fixed game displaying wrong players turn

--- Game.java
+++ Game.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Game {
 
-    private static char turn;
+    private static char turn = 'o';
 
     public static void showBoard(Board board) {
         System.out.println("|" + board.getState().substring(0, 3) + "|");
@@ -11,13 +11,8 @@ public class Game {
     }
 
     public static Board playerMakesMove(Board board, int pos) {
-        if (turn == 'x')  {
-            turn = 'o';
-        } else {
-            turn = 'x';
-        }
         System.out.println("Its your turn " + turn + "!");
-        System.out.println("Where would you like to go? (1, 2, 3, 4, 5, 6, 7, 8, 9)");
+        turn = (turn == 'x' ? 'o' : 'x');
         board = board.playerMakesMove(turn, pos - 1);
         showBoard(board);
         return board;
@@ -27,7 +22,6 @@ public class Game {
         Scanner s = new Scanner(System.in);
         Board board = new Board("---------");
         showBoard(board);
-        System.out.println("Its your turn X");
         System.out.println("Where would you like to go? (1, 2, 3, 4, 5, 6, 7, 8, 9)");
         while (!board.checkForDraw()) {
             board = playerMakesMove(board, s.nextInt());
