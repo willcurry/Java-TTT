c387db4a6717c81820bd6c3e03eae7763154024f
willcurry <willcurry@Wills-MBP.default>
2016-07-25 13:10:09 +0100

    Added a test for Game class

--- GameTests.java
+++ GameTests.java
@@ -5,5 +5,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class GameTests {
-
+    @Test
+    public void playerMoveIsRegistered() {
+        Game game = new Game(System.in);
+        Board board = new Board("---------");
+        assertThat(game.playerMakesMove(board, 2).getState(), is("-x-------"));
+    }
 }
